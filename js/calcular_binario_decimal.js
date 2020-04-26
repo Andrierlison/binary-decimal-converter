af0a55b0bb1553206b12f6f2cb3238ea53e88ab7
Andrierlison <andrierlisonsilva8@gmail.com>
2020-04-26 06:58:02 -0300

    Remoção do resultado NaN

--- js/calcular_binario_decimal.js
+++ js/calcular_binario_decimal.js
@@ -1,11 +1,20 @@
-function converterParaDecimal(){
-	var decimal = document.getElementById("decimal").value
-	decimal = parseInt(decimal)
-	binario = decimal.toString(2);
-	document.getElementById("resultadoBinario").innerHTML = binario
+function converterParaDecimal() {
+  var decimal = document.getElementById("decimal").value
+  decimal = parseInt(decimal)
+  binario = decimal.toString(2);
+  if (isNaN(binario)) {
+    document.getElementById("resultadoBinario").innerHTML = 'Aguardando a inserção dos números...'
+  } else {
+    document.getElementById("resultadoBinario").innerHTML = binario
+  }
 }
-function converterParaBinario(){
-	var binario = document.getElementById("binario").value
-	var decimal = parseInt(binario, 2);
-	document.getElementById("resultadoDecimal").innerHTML = decimal
+
+function converterParaBinario() {
+  var binario = document.getElementById("binario").value
+  var decimal = parseInt(binario, 2);
+  if (isNaN(decimal)) {
+    document.getElementById('resultadoDecimal').innerHTML = 'Por favor digite os valores em 0 e 1...'
+  } else {
+    document.getElementById("resultadoDecimal").innerHTML = decimal
+  }
 }
